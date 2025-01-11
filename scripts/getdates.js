265292358b35133486656606980d8c3ef6766d69
Ronaldoryne <rryne2001@gmail.com>
2025-01-11 22:23:20 +0200

    add

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const copyrightYear = new Date().getFullYear();
+  document.querySelector("footer p:first-child").textContent = `Copyright ${copyrightYear}`;
+  const lastModified = document.lastModified;
+  document.querySelector("footer p:nth-child(2)").textContent = `Last modified: ${lastModified}`;
+});
+
+
